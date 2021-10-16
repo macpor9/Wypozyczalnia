ba037823f40edb37a9a91d9b19f1204eb7e1b360
Maciej Poreba <macpor9@gmail.com>
2021-10-16 17:04:19 +0200

    add auth service module

--- backend/src/main/java/com/maciej/poreba/backend/BackendApplication.java
+++ backend/src/main/java/com/maciej/poreba/backend/BackendApplication.java
@@ -1,9 +1,15 @@
 package com.maciej.poreba.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableMongoRepositories
+@Slf4j
 public class BackendApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/maciej/poreba/backend/authservice/DefaultConfig.java
@@ -0,0 +1,18 @@
+package com.maciej.poreba.backend.authservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
+
+@Configuration
+public class DefaultConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
+
+    @Bean
+    public Random random(){return new Random();}
+
+}
