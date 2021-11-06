e5a60addfd4ca0de7ddd753f990ccd600a318508
Maciej Poreba <macpor9@gmail.com>
2021-11-06 21:40:50 +0100

    change registration form fields order

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,6 +19,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from './authorization/pages/register/register.component';
 import { FacebookLoginButtonComponent } from './authorization/components/facebook-login-button/facebook-login-button.component';
 import { GoogleLoginButtonComponent } from './authorization/components/google-login-button/google-login-button.component';
+import { AdminPanelComponent } from './application/pages/admin-panel/admin-panel.component';
+import { TopBarComponent } from './application/components/top-bar/top-bar.component';
 
 
 @NgModule({
@@ -30,6 +32,8 @@ import { GoogleLoginButtonComponent } from './authorization/components/google-lo
     RegisterComponent,
     FacebookLoginButtonComponent,
     GoogleLoginButtonComponent,
+    AdminPanelComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
